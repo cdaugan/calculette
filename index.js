0cdfa4ced1802b275ad90fd63212e59b9b15ddad
Fabrice Daugan <fabrice.daugan@gfi.fr>
2020-03-07 16:19:07 +0100

    réglage des points et de la couleur

--- index.js
+++ index.js
@@ -21,11 +21,8 @@ $(function () {
         }
     ];
 
-    function poserQuestion(question) {
-        $('#question').text(question);
-    }
 
-    function nouvelleQuestion() {
+    function genererQuestion() {
         let op1 = (Math.floor(Math.random() * 9) + 1);
         let operation = (Math.floor(Math.random() * 3));
         let op2 = (Math.floor(Math.random() * 9) + 1);
@@ -36,24 +33,42 @@ $(function () {
         };
     }
 
+    let score = 0;
+    let nbQuestions = 0;
 
-    let question = nouvelleQuestion();
-    poserQuestion(question.op1 + operations[question.operation].nom + question.op2 + ' =');
+    function afficherQuestion(question) {
+        $('#question').text(question);
+        $('#score').text(score);
+        $('#scorePourcent').text(nbQuestions == 0 ? '0% - #' : Math.floor((score/nbQuestions*100 )) + '% - #');
+    }
 
+    let question = null;
 
+    function nouvelleQuestion() {
+        question = genererQuestion();
+        afficherQuestion(question.op1 + operations[question.operation].nom + question.op2 + ' =');
+        $('#reponse').trigger('focus');
+        $('#reponse').val('');
+    }
 
     $('#reponse-decor').on('submit', function (e) {
         e.preventDefault();
         let reponse = $('#reponse').val();
         console.log('Ta réponse est ' + reponse);
         let bonneReponse = operations[question.operation].calcul(question.op1, question.op2);
+        $('#reponse').removeClass("bonne").removeClass("mauvaise");
         if (reponse == bonneReponse) {
             console.log('Bravo bonne réponse');
+            $('#reponse').addClass("bonne");
+            score++; // score = score + 1; score += 1;
         } else {
             console.log('Désolé, la bonne réponse était ' + bonneReponse);
-
+            $('#reponse').addClass("mauvaise"); 
         }
+        nbQuestions++;
+        nouvelleQuestion();
     })
 
-
+    // Initialisation : première question
+    nouvelleQuestion();
 })
