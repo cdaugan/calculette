6c707471f69c73729625661015bc2397ac09cedc
Fabrice Daugan <fabrice.daugan@gfi.fr>
2020-10-31 11:43:23 +0100

    couleur du fond et sons

--- index.js
+++ index.js
@@ -39,10 +39,19 @@ $(function () {
     function afficherQuestion(question) {
         $('#question').text(question);
         $('#score').text(score);
-        $('#scorePourcent').text(nbQuestions == 0 ? '0% - #' : Math.floor((score/nbQuestions*100 )) + '% - #');
+        $('#scorePourcent').text(nbQuestions == 0 ? '0% - #' : Math.floor((score / nbQuestions * 100)) + '% - #');
     }
 
     let question = null;
+
+    function play(type) {
+        var audio = $('#audio-' + type)[0];
+        audio.play();
+        setTimeout(function () {
+            audio.pause();
+            audio.currentTime = 0;
+        }, 1000)
+    }
 
     function nouvelleQuestion() {
         question = genererQuestion();
@@ -56,14 +65,20 @@ $(function () {
         let reponse = $('#reponse').val();
         console.log('Ta réponse est ' + reponse);
         let bonneReponse = operations[question.operation].calcul(question.op1, question.op2);
-        $('#reponse').removeClass("bonne").removeClass("mauvaise");
         if (reponse == bonneReponse) {
             console.log('Bravo bonne réponse');
-            $('#reponse').addClass("bonne");
+            // $('#reponse').addClass("bonne");
+            $('body').removeClass("mauvaise");
+            $('body').addClass("bonne");
+            play("bonne") ;
+
             score++; // score = score + 1; score += 1;
         } else {
             console.log('Désolé, la bonne réponse était ' + bonneReponse);
-            $('#reponse').addClass("mauvaise"); 
+            // $('#reponse').addClass("mauvaise"); 
+            $('body').removeClass("bonne");
+            $('body').addClass("mauvaise");
+            play("mauvaise") ;
         }
         nbQuestions++;
         nouvelleQuestion();
@@ -71,4 +86,11 @@ $(function () {
 
     // Initialisation : première question
     nouvelleQuestion();
+
+    $("#bouton-bonne").on("click", function () {
+        play("bonne");
+    });
+    $("#bouton-mauvaise").on("click", function () {
+        play("mauvaise");
+    });
 })
